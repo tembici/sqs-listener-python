0bc87027eecc821f5166fb63e332ff4dd3d2c054
Deepak Vashist <deepaksvashist@gmail.com>
2020-06-12 10:33:27 -0300

    Create project initial structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# SQS Listener Python
+
+## Using pre-commit
+
+pre-commit is a group os scripts that are added in the `pre-commit` hook in your local git repo. What means that every time you type `git commit` those script will be executed, checking and improving the code quality and pattern.
+
+More information at: https://pre-commit.com/
+
+1) Install pre-commit following this guide: https://pre-commit.com/#installation
+2) Install the hooks on your project: In your local repository, run: `pre-commit install`
+3) Test if it's working fine running `pre-commit run`
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sqs-listener-python",
+    version="1.0.0",
+    url="https://github.com/tembici/sqs-listener-python",
+    description="",
+    long_description="",
+    author="Tembici",
+    author_email="falecomagente@tembici.com.br",
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=3.6",
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
