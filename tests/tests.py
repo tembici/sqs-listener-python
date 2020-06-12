37a61509c8cb41d275ec2fd91310d5a2292c10fc
Marcelo Theodoro <marcelotheodoro@outlook.com>
2020-06-12 10:42:25 -0300

    Add basic CI to check tests and coverage

---
+++ tests/tests.py
@@ -0,0 +1,8 @@
+
+from unittest import TestCase
+
+
+class TempTestCase(TestCase):
+    def test_true_is_always_true(self):
+        """Just a mock test to be sure the CI is fine."""
+        self.assertTrue(True)
