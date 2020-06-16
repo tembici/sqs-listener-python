dcbc0a68e50ef47f3dbb2b17d3d0113d85f54f79
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-16 16:02:37 -0300

    update format unit test

--- tests/tests_sqs_listener.py
+++ tests/tests_sqs_listener.py
@@ -37,7 +37,7 @@ class SQSListenerTestCase(TestCase):
         sqs_message = SQS_RESPONSE_MULTIPLE_MESSAGES["Messages"][0]
 
         expected_response = {"id": "001"}
-        actual_response = self.sqs.format_sqs_message_to_pbsc_event(sqs_message)
+        actual_response = self.sqs.pbsc_format(sqs_message)
         self.assertEqual(actual_response, expected_response)
 
     @patch("SQSListener.listener.SQSListener.mark_message_to_be_deleted")
